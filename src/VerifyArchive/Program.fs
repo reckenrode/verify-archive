41f1626c80f9e35da9f3d016d4f24d89a473144e
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-02 11:39:27 -0400

    Initial commit

---
+++ src/VerifyArchive/Program.fs
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
