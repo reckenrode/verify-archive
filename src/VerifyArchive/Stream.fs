0e853e9052bd0a4c4c5be52004fe305dfbc7f571
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-26 19:17:54 -0400

    Use Blake3 instead of SHA-256 to hash file contents

--- src/VerifyArchive/Stream.fs
+++ src/VerifyArchive/Stream.fs
@@ -5,8 +5,9 @@ module VerifyArchive.Stream
 #nowarn "0025"
 
 open FSharp.Control
+open FSharp.Control.Tasks.NonAffine.Unsafe
 open System.IO
-open System.Security.Cryptography
+open System
 open System.Text
 
 let private hashToString hash =
@@ -14,16 +15,32 @@ let private hashToString hash =
     |> Array.fold (fun (sb: StringBuilder) b -> sb.Append $"{b:x2}") (StringBuilder ())
     |> string
 
+let private computeHashAsync (stream: Stream) = uvtask {
+    let backingArray = GC.AllocateUninitializedArray (512 * 1024)
+    let buffer = Memory backingArray
+    use hasher = Blake3.Hasher.New ()
+
+    let! asyncBytesRead = stream.ReadAsync buffer
+    let mutable bytesRead = asyncBytesRead
+    while bytesRead > 0 do
+        let span = ReadOnlySpan (backingArray, 0, bytesRead)
+        if bytesRead < (128 * 1024)
+        then hasher.Update span
+        else hasher.UpdateWithJoin span
+        let! asyncBytesRead = stream.ReadAsync buffer
+        bytesRead <- asyncBytesRead
+
+    return hasher.Finalize ()
+}
+
 let matches (backupStream: Stream) (sourceStream: Stream) = async {
-    use sourceHasher = SHA256.Create ()
-    use backupHasher = SHA256.Create ()
-    let hashers =
+    let hashes =
         [|
-            sourceHasher.ComputeHashAsync sourceStream |> Async.AwaitTask;
-            backupHasher.ComputeHashAsync backupStream|> Async.AwaitTask
+            (computeHashAsync sourceStream).AsTask () |> Async.AwaitTask;
+            (computeHashAsync backupStream).AsTask () |> Async.AwaitTask
         |]
         |> Async.Parallel
-    let! hashers = hashers
-    let [| sourceHash; backupHash |] = hashers
+    let! hashes = hashes
+    let [| sourceHash; backupHash |] = hashes
     return sourceHash = backupHash
 }
