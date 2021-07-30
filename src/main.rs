720771984d2186ed47a0a0a4acec537ad7c10823
Randy Eckenrode <randy@largeandhighquality.com>
2021-07-29 21:45:17 -0400

    Implement main driver

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use clap::Clap;
+use tokio::io;
 
+use crate::commands::verify;
+
+mod commands;
 mod digest;
 mod fs;
 mod opts;
 mod zip;
 
-fn main() {
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> io::Result<()> {
     let opts = opts::Opts::parse();
+    verify(opts).await
 }
