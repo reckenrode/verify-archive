3fbb556a5ab73b0e70779930296759c969c22335
Randy Eckenrode <randy@largeandhighquality.com>
2023-01-18 13:32:18 -0500

    verify-archive: 0.6.0 → 0.6.1

--- default.nix
+++ default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "verify-archive";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "reckenrode";
     repo = "verify-archive";
     rev = "v${version}";
-    hash = "sha256-6Ctucvw/t75ugK0+K04/kBTecYY2lafWZfNTsnitRjU=";
+    hash = "sha256-XyKibEaDnA4UsGHz2Vq3Xvf6zi9MbXiHDjTkDAKCCjo=";
   };
 
-  cargoHash = "sha256-UYOxL0udArggNGPe60spR4CfsXrFEtOfpmq8MDw4gmk=";
+  cargoHash = "sha256-yXXLMfTcC+IPwHgxLIU5uLdhTol+Lf1HhV7+9vBpnCY=";
 
   meta = let inherit (lib) licenses platforms; in {
     description = "Compare Backblaze backup archives to the local filesystem for discrepancies";
